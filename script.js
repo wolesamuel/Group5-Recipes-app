0509e04e051f73e31d54a6cc92c0e7f5fdd923f5
Faith2Felix <faithfelix1998@gmail.com>
2026-06-15 07:46:28 +0100

    SearchPage update

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+   const menuBtn = document.getElementById("menuBtn");
+      const closeBtn = document.getElementById("closeBtn");
+      const mobileSidebar = document.getElementById("mobileSidebar");
+
+      menuBtn.addEventListener("click", () => {
+        mobileSidebar.classList.remove("left-[-100%]");
+        mobileSidebar.classList.add("left-0");
+      });
+
+      closeBtn.addEventListener("click", () => {
+        mobileSidebar.classList.remove("left-0");
+        mobileSidebar.classList.add("left-[-100%]");
+      });
